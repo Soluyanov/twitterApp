8bf83ac648ad04378137e4ed878c7de256c90b4f
Soluyanov <siluyanov.a.v@gmail.com>
2017-02-28 14:48:56 +0300

    format code

--- src/main/scala/com/test/core/streaming/Fake.scala
+++ src/main/scala/com/test/core/streaming/Fake.scala
@@ -1,120 +1,64 @@
 package com.test.core.streaming
 
-import org.apache.hadoop.io.SequenceFile.CompressionType
-import org.apache.hadoop.io.compress.CompressionCodec
 import org.apache.spark._
-import org.apache.spark.SparkConf
-import org.apache.spark.streaming._
-import org.apache.spark.streaming.kafka._
-import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.{FileSystem, FileUtil, Path}
-import org.apache.hadoop.io.NullWritable
 import org.apache.log4j.Logger
-import org.apache.hadoop.mapred.lib.MultipleTextOutputFormat
-import org.apache.hadoop.mapred.JobConf
 import org.apache.spark.SparkContext
-import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
-import org.apache.spark.streaming.Milliseconds
-import org.apache.spark.streaming.StreamingContext._
-import org.apache.spark.storage.StorageLevel
-import org.apache.spark.streaming.{Seconds, StreamingContext}
-import org.apache.spark.streaming.dstream._
-import org.apache.hadoop.mapred.SequenceFileOutputFormat;
-import org.apache.hadoop.io.{
-  ArrayWritable,
-  BooleanWritable,
-  BytesWritable,
-  DoubleWritable,
-  FloatWritable,
-  IntWritable,
-  LongWritable,
-  NullWritable,
-  Text,
-  Writable
-}
-import org.apache.spark.rdd.PairRDDFunctions
-import scala.collection.mutable.ListBuffer
-import org.apache.hadoop.io.compress.GzipCodec
-import java.io.PrintWriter
-import org.apache.hadoop.fs.FSDataOutputStream
-import java.io._
-import org.apache.spark.sql.SQLContext
-import org.apache.spark.sql.hive.HiveContext
-import scala.io.Source
-import org.apache.spark.sql.functions.unix_timestamp
-import java.net.{InetAddress, ServerSocket, Socket, SocketException}
-import org.apache.hadoop.fs.Path
-import scala.collection.mutable.ArrayBuffer
 import org.apache.hadoop.hbase.HBaseConfiguration
-import org.apache.hadoop.hbase.mapred.TableOutputFormat
 import org.apache.hadoop.hbase.client.Put
 import org.apache.hadoop.hbase.client.HTable
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat
-import org.apache.spark.streaming.twitter._
 import twitter4j._
-import twitter4j.auth.AuthorizationFactory
-import org.apache.spark.streaming.api.java.JavaDStream
-import org.apache.spark.streaming.api.java.JavaPairDStream
-import org.apache.spark.streaming.api.java.JavaReceiverInputDStream
-import org.apache.spark.streaming.api.java.JavaStreamingContext
-import org.apache.hadoop.hbase.HBaseConfiguration
-import org.apache.hadoop.hbase.mapred.TableOutputFormat
-import org.apache.hadoop.hbase.client.Put
-import org.apache.hadoop.hbase.client.HTable
-import org.apache.hadoop.hbase.mapreduce.TableInputFormat
 
 /**
-  * Application used to write rsyslog messages to hdfs
+  * Application used to write twitter messages to Hbase
   */
 object Fake {
 
   val Log = Logger.getLogger(Fake.this.getClass().getSimpleName())
   def main(args: Array[String]) {
-    if (args.length < 4) {
+    if (args.length < 6) {
       System.err.println(
-        "Usage: KafkaWordCountProducer <metadataBrokerList> <topic> " +
-          "<messagesPerSec> <wordsPerMessage>")
+        "Usage: consumerKey, consumerSecret, accessToken" +
+          "tokenSecret, tableName, userId")
       System.exit(1)
     }
 
-    val Array(zkQuorum, group, topics, numThreads, source, pathToStore) = args
+    val Array(consumerKey,
+              consumerSecret,
+              accessToken,
+              tokenSecret,
+              tableName,
+              userId) = args
     val sparkConf = new SparkConf().setAppName("Fake")
     val sc = new SparkContext(sparkConf)
-/**
-     sc.setLogLevel("WARN")
-    val ssc = new StreamingContext(sc, Seconds(60))
-*/
 
-  
-    /**
-      * Writes messages in files and specifies the behavior, when target
-      * file exists or not
-      */
-
-
-  val config = new   twitter4j.conf.ConfigurationBuilder()
-    .setOAuthConsumerKey("7GWZiG3xSSVXUpoXhLAdTwcr4")
-    .setOAuthConsumerSecret("Cqmm3WFwP4Z8sV0payW9CsYnKOWZYzhr1NrNYD49PTeVrd5f0L")
-    .setOAuthAccessToken("376173910-Vms6JpjWjdXcYbMQrlXQ5hhN7QSlUrK17rGgrdlb")
-    .setOAuthAccessTokenSecret("pMqqXGpLhBMyyesCbxQtyaGLYs3ysoH2RuuZf7m6yHUFA")
-    .build
+    val config = new twitter4j.conf.ConfigurationBuilder()
+      .setOAuthConsumerKey(consumerKey)
+      .setOAuthConsumerSecret(consumerSecret)
+      .setOAuthAccessToken(accessToken)
+      .setOAuthAccessTokenSecret(tokenSecret)
+      .build
 
     val conf = HBaseConfiguration.create()
-    val tableName = "t1"
+    conf.set("zookeeper.znode.parent", "/hbase-unsecure")
     conf.set(TableInputFormat.INPUT_TABLE, tableName)
     val myTable = new HTable(conf, tableName)
 
+    /** Writes twitter message to Hbase */
+    def putTweetToHbase(columnFamily: String,
+                        columnName: String,
+                        tweet: String) = {
+      var p = new Put(System.currentTimeMillis().toString.getBytes())
+      p.add(columnFamily.getBytes(), columnName.getBytes(), tweet.getBytes)
+      myTable.put(p) //ask a question
+      myTable.flushCommits()
+
+    }
 
     def simpleStatusListener = new StatusListener() {
       def onStatus(status: Status) {
-        println(status.getText + " " + status.getUser)
-
-        var p = new Put(System.currentTimeMillis().toString.getBytes())
-        p.add("cf".getBytes(), "column_name".getBytes(), new String(status.getText).getBytes())
-        myTable.put(p)
-        myTable.flushCommits()
-
-
+        println(status.getText)
+        putTweetToHbase("cf", "column_name", status.getText)
       }
       def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice) {}
       def onTrackLimitationNotice(numberOfLimitedStatuses: Int) {}
@@ -125,44 +69,7 @@ object Fake {
 
     val twitterStream = new TwitterStreamFactory(config).getInstance
     twitterStream.addListener(simpleStatusListener)
-    twitterStream.filter(new FilterQuery(Array(376173910)))
-   // Thread.sleep(60000)
-   // twitterStream.cleanUp
-   // twitterStream.shutdown
-
-
-/**
-    System.setProperty("twitter4j.oauth.consumerKey", "7GWZiG3xSSVXUpoXhLAdTwcr4")
-System.setProperty("twitter4j.oauth.consumerSecret", "Cqmm3WFwP4Z8sV0payW9CsYnKOWZYzhr1NrNYD49PTeVrd5f0L")
-System.setProperty("twitter4j.oauth.accessToken", "376173910-Vms6JpjWjdXcYbMQrlXQ5hhN7QSlUrK17rGgrdlb")
-System.setProperty("twitter4j.oauth.accessTokenSecret", "pMqqXGpLhBMyyesCbxQtyaGLYs3ysoH2RuuZf7m6yHUFA")
-
-    val stream = TwitterUtils.createStream(ssc, None)
-
-
-    val tweets = stream.filter {t =>
-      val tags = t.getText.split(" ").filter(_.startsWith("#")).map(_.toLowerCase)
-      tags.contains("#bigdata") && tags.contains("#oscar")
-    }
-
-             tweets.saveAsTextFiles("/home/alexander/Downloads/tweet")
-*/
-/**
-    Log.error("DEBUG info:" + zkQuorum)
-    sys.ShutdownHookThread({
-      println("Ctrl+C")
-      try {
-        ssc.stop(stopSparkContext = true, stopGracefully = true)
-      } catch {
-        case e: Throwable => {
-          println("exception on ssc.stop(true, true) occured")
-        }
-      }
-    })
-    ssc.start()
-    ssc.awaitTermination()
-*/
-
+    twitterStream.filter(new FilterQuery(Array(userId.toLong)))
 
   }
 }
